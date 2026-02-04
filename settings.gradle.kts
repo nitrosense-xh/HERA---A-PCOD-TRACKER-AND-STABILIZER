1006576bb2395aa80ce4de2a0f4c5516225e0e65
nitrosense-xh <akashjaguar2004@gmail.com>
2026-02-04 15:19:08 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode.*
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+
+
+    }
+}
+
+rootProject.name = "HERA 1.2"
+include(":app")
+ 
